ca48f3376c5775e010564dc026c74077a6385327
Felipe Leal <felipe.leal.sierra@gmail.com>
2020-06-26 23:25:16 -0600

    Added the function to find the start of a word and the function to find the end of a word.

--- src/tokenizer.c
+++ src/tokenizer.c
@@ -13,8 +13,9 @@ int string_length(char* string){
     return counter; 
 }
 
+/*Evaluates weather a character is valid or invalid*/
 char is_valid_character(char c){
-    switch(c){
+    switch(c){     //switch method allows you to try different cases
         case '\v':
         case '\\':
         case '\'':
@@ -26,12 +27,46 @@ char is_valid_character(char c){
         case '\b':
         case '\a':
         case '\f':
-        case '\r':
+        case '\r': 
             return 0;
         default:
             return 1;
     }
 }
+
+/*Function to find the start of a word*/
+int find_word_start(char* sentence, int index){
+    while(*sentence){
+        if(is_valid_character(sentence[index]) == 1){
+            return index;
+        }
+        else{
+            index++;
+        }
+        return index;
+    }
+}
+
+/*Function to find the end of a word*/
+int find_word_end(char* sentence, int index){
+    while(*sentence){
+        if(is_valid_character(sentence[index]) == 0){
+            return index;
+        }
+        else{
+            index++;
+        }
+        return index;
+    }
+}
+
+
+
+
+
+
+
+
 
 
 
