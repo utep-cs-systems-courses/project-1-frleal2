93384f541ffd4f74abde7a75f61b08973fcfa0c5
Felipe Leal <felipe.leal.sierra@gmail.com>
2020-06-28 21:29:32 -0600

    UI

--- src/UI.c
+++ src/UI.c
@@ -11,34 +11,23 @@ int main(){
   read_command();
 }
 
-void read_command(){
+void read_comms(){
   char c;
-  char command[50];
+  char com[100];
   
   while(1){
-    printf("Welcome to Tokenizer by Felipe Leal, read the README in order to use this tool or type /h for help, have fun :)");
-    printf("#");
-    scanf("%s", command);
-    if(command[0]=='/'){
-      if(command[1] == 'e'){
-          printf("Exiting program");
-          break;
-      }
-      else if(command[1] == 't'){
-          read_tokens();
-      }
-      else if(command[1] == 'h'){
-          printf("/e: will exit the program.");
-          printf("/t: will enter tokenize mode.");
-          printf("/h: will print tokenizer commnads (HELP).");
-          printf("any input: will be echoed.");
-      }
-      else{
-          printf("Command not recognized");
-      }
+    printf(">>>");
+    scanf("%s", com);
+    if(com[0]=='-'){
+      if(com[1] == 'q')
+	break;
+      else if(com[1] == 't')
+	tokenizing_mode();
+      else
+	printf("Command not recognized");
     }
     else{
-      printf("%s \n", command);
+      printf("%s \n", com);
     }
     
   }
